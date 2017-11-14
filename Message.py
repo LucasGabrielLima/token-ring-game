64529609d0b13d4ffa1484fba24dea10163c5602
Lucas Gabriel Lima <lgl15@inf.ufpr.br>
2017-11-14 00:21:42 -0200

    update Message.py

---
+++ Message.py
@@ -0,0 +1,20 @@
+class Message (object):
+    start = "start"
+    token = ""
+    control = ""
+    origin = ""
+    dest = ""
+    x = ""
+    y = ""
+    seen = False
+    hit = False
+    miss = False
+    kill = False
+
+    def __init__(self, token, control, dest, x = "", y = ""):
+        self.token = token
+        self.control = control
+        self.origin = socket.gethostname
+        self.dest = dest
+        self.x = x
+        self.y = y
