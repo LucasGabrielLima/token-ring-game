dbcc559cc6ccc879906043822a9f77e6ad31986e
Lucas Gabriel Lima <lgl15@inf.ufpr.br>
2017-11-14 19:20:53 -0200

    print field

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 import socket
 class message (object):
-    def __init__(self, token, control, dest, x = "", y = ""):
+    def __init__(self, token, control, dest, x = "", y = "", orientation = 'v'):
         self.start = "start"
         self.token = token
         self.control = control
@@ -8,6 +8,7 @@ class message (object):
         self.dest = dest
         self.x = x
         self.y = y
+        self.orientation = orientation
         self.seen = False
         self.hit = False
         self.miss = False
@@ -15,7 +16,19 @@ class message (object):
 
 class create(object):
     def __init__(self):
-        self.field = [[0 for x in range(5)] for y in range(5)] # Inicializa uma matriz 5x5, preenchida com
+        self.field = [[0 for x in range(5)] for y in range(5)] # Inicializa uma matriz 5x5, preenchida com 0s
         players = ''
         kills = 0
         ships = 2
+
+    def printField(self):
+        for x in range(5):
+            for y in range(5):
+                print('|' + str(self.field[x][y]) + '|'),
+            print('')
+            print('-------------------')
+
+
+
+game = create()
+game.printField()
