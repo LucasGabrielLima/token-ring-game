008a22160542b4a17025ba1a28539c2c639a4adf
Lucas Gabriel Lima <lgl15@inf.ufpr.br>
2017-11-14 19:06:39 -0200

    create game class and fix classes

--- Game.py
+++ Game.py
@@ -1,21 +1,21 @@
 import socket
 class message (object):
-    start = "start"
-    token = ""
-    control = ""
-    origin = ""
-    dest = ""
-    x = ""
-    y = ""
-    seen = False
-    hit = False
-    miss = False
-    kill = False
-
     def __init__(self, token, control, dest, x = "", y = ""):
+        self.start = "start"
         self.token = token
         self.control = control
         self.origin = socket.gethostname
         self.dest = dest
         self.x = x
         self.y = y
+        self.seen = False
+        self.hit = False
+        self.miss = False
+        self.kill = False
+
+class create(object):
+    def __init__(self):
+        self.field = [[0 for x in range(5)] for y in range(5)] # Inicializa uma matriz 5x5, preenchida com
+        players = ''
+        kills = 0
+        ships = 2
